2063b4e3c27932473d6c7514ae8be516ee9ce2bb
James Dorsey <james.dorsey@vtsd.com>
2022-01-21 07:58:43 -0500

    buy my bikes

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,9 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
-	
-}
+	function show(){
+		var imageTask1 = document.getElementById("buttonTask1");
+		imageTask1.style.display = "block";
+	}
+	function byeBye() {
+		var buttonTask1 = document.getElementById("imageTask1");
+		buttonTask1.style.display = "none";
+	}
