99758c9a7bad56e91308756a5b8f9208a73aee88
James Dorsey <james.dorsey@vtsd.com>
2022-01-26 08:26:58 -0500

    buy my bikes

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,15 @@
 /* Put code for the task below */
+function turnBlue(){
+    var blue = document.getElementById("table2");
+    blue.style.backgroundColor="blue";
+}
+
+function turnRed(){
+    var red = document.getElementById("table2");
+    red.style.backgroundColor="red";
+}
+
+function turnGreen(){
+    var green = document.getElementById("table2");
+    green.style.backgroundColor="green";
+}
--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,1 +1,4 @@
 /* Put code for the task below */
+function change(){
+    var old = document.getElementById("old").innerHTML="I Clicked It";
+}
